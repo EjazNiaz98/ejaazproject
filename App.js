631ad9152b72a27a728cc24fc6d8c70be7e85cde
Ejaz Niaz <ejazniaz98@gmail.com>
2020-09-24 19:24:41 -0700

    first commit

--- App.js
+++ App.js
@@ -3,9 +3,12 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const clickFunction = () =>{
+    alert('hello pakistan');
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+  <button onClick={clickFunction}>onPress</button>
       <StatusBar style="auto" />
     </View>
   );
